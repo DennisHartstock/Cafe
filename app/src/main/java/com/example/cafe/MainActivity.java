9813b9d90d640ce7716514c81adfde52a7237115
DennsHartstock <dennis.hartstock@web.de>
2022-11-20 10:45:00 +0100

    add layout of MakeOrderActivity

--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.cafe;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btLogin = findViewById(R.id.btLogin);
+
+        btLogin.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MakeOrderActivity.class);
+            startActivity(intent);
+        });
     }
 }

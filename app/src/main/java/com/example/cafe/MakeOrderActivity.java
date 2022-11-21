25fe9707c99628e225f09a18325c8b41acba4636
DennsHartstock <dennis.hartstock@web.de>
2022-11-21 08:16:06 +0100

    add layout of OrderDetailActivity

--- app/src/main/java/com/example/cafe/MakeOrderActivity.java
+++ app/src/main/java/com/example/cafe/MakeOrderActivity.java
@@ -1,14 +1,24 @@
 package com.example.cafe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MakeOrderActivity extends AppCompatActivity {
+    private Button btMakeOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order);
+
+        btMakeOrder = findViewById(R.id.btMakeOrder);
+
+        btMakeOrder.setOnClickListener(view -> {
+            Intent intent = new Intent(this, OrderDetailActivity.class);
+            startActivity(intent);
+        });
     }
 }

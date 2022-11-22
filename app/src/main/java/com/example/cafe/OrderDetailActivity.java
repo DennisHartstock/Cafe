cb1be4f3eabd3d8b3bc4afbf3a99c6c0bb3bfb1d
DennsHartstock <dennis.hartstock@web.de>
2022-11-22 16:21:53 +0100

    add logic of OrderDetailActivity

--- app/src/main/java/com/example/cafe/OrderDetailActivity.java
+++ app/src/main/java/com/example/cafe/OrderDetailActivity.java
@@ -3,6 +3,7 @@ package com.example.cafe;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,10 +14,17 @@ public class OrderDetailActivity extends AppCompatActivity {
     public static final String EXTRA_ADDITIVES = "additives";
     public static final String EXTRA_DRINK_TYPE = "drinkType";
 
+    private TextView tvNameValue;
+    private TextView tvDrinkValue;
+    private TextView tvDrinkTypeValue;
+    private TextView tvAdditivesValue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+        initViews();
+        setupTextViews();
     }
 
     public static Intent newIntent(
@@ -32,5 +40,26 @@ public class OrderDetailActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_DRINK_TYPE, drinkType);
         intent.putExtra(EXTRA_ADDITIVES, additives);
         return intent;
+    }
+
+    private void initViews() {
+        tvNameValue = findViewById(R.id.tvNameValue);
+        tvDrinkValue = findViewById(R.id.tvDrinkValue);
+        tvDrinkTypeValue = findViewById(R.id.tvDrinkTypeValue);
+        tvAdditivesValue = findViewById(R.id.tvAdditivesValue);
+    }
+
+    private void setupTextViews() {
+        Intent intent = getIntent();
+
+        String name = intent.getStringExtra(EXTRA_NAME);
+        String drink = intent.getStringExtra(EXTRA_DRINK);
+        String drinkType = intent.getStringExtra(EXTRA_DRINK_TYPE);
+        String additives = intent.getStringExtra(EXTRA_ADDITIVES);
+
+        tvNameValue.setText(name);
+        tvDrinkValue.setText(drink);
+        tvDrinkTypeValue.setText(drinkType);
+        tvAdditivesValue.setText(additives);
     }
 }
